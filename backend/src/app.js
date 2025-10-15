fd023aef825531148830d2ed3690042a3c41a260
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-10-15 18:44:09 +0530

    landingpage Created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
 const authRoutes = require('./routes/auth.routes');
 
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use('/api/auth', authRoutes);
 module.exports= app;
 
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+    <div className='Nav h-20 w-full  px-20 flex items-center'>
+        <h1 className='text-white font-semibold text-5xl font-mono'>Vireon.</h1>
+        <div className='h-full w-full flex justify-end items-center '>
+           <ul className='text-lg text-white flex gap-5'>
+            <li className='py-1 px-8 bg-[#000f5279] cursor-pointer hover:bg-[#000F52] rounded-3xl'>Platform</li>
+            <li className='py-1 px-8 bg-[#000f5279] cursor-pointer hover:bg-[#000F52] rounded-3xl'>About Us</li>
+            <li className='py-1 px-8 bg-[#000F5279] cursor-pointer hover:bg-[#000F52] rounded-3xl'>Services</li>
+            <li className='py-1 px-8 bg-[#000F5279] cursor-pointer hover:bg-[#000F52] rounded-3xl'>Metavers</li>
+            <li className='py-1 px-8 bg-[#000F5279] cursor-pointer hover:bg-[#000F52] rounded-3xl'>Contact</li>
+           </ul>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Nav
